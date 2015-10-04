1a3feda5bab0a854365219ce83cea815b9794c97
Raymond Tang <raymond@tang.me>
2015-10-03 20:26:58 -0400

    initial commit of timetracker

---
+++ test_timetracker_analyzer.py
@@ -0,0 +1,11 @@
+import unittest
+import timetracker_analyzer as analyzer
+
+class TestTimeTrackerAnalyzer(unittest.TestCase):
+
+    def test_total_up_tasks(self):
+        self.assertEqual(None,
+                         analyzer.total_up_tasks('test', '00:00', '00:00'))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ timetracker_analyzer.py
@@ -0,0 +1,13 @@
+import csv
+
+
+def total_up_tasks(task_name, start_time, end_time):
+    """Total up the time for the specific task"""
+    pass
+
+
+def analyze_data(filename):
+    """For a given file, analyze the file and return back a dictionary object
+    composed of months, tasks within that month, and total time for each task.
+    """
+    pass
