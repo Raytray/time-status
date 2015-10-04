1d1023b2ebfca8cfd1b72c83e3db73f142dfa066
Raymond Tang <raymond@tang.me>
2015-10-04 12:38:57 -0400

    add get_time_delta
    * fills in logic for get_time_delta
    * renames total_up_tasks method
    * tests negative, none, and perfect cases

--- test_timetracker_analyzer.py
+++ test_timetracker_analyzer.py
@@ -1,12 +1,28 @@
 import unittest
 import timetracker_analyzer as analyzer
 
+from datetime import timedelta
+
 
 class TestTimeTrackerAnalyzer(unittest.TestCase):
 
-    def test_total_up_tasks(self):
-        self.assertEqual(None,
-                         analyzer.total_up_tasks('test', '00:00', '00:00'))
+    def test_get_time_delta_perfect(self):
+        expected = timedelta(seconds=9)
+        self.assertEqual(expected,
+                         analyzer.get_time_delta("2015-10-03 19:08:30",
+                                                 "2015-10-03 19:08:39"))
+
+    def test_get_time_delta_empty(self):
+        self.assertRaises(ValueError,
+                          analyzer.get_time_delta,
+                          "2015-10-03 19:08:30",
+                          "")
+
+    def test_get_time_delta_negative(self):
+        self.assertRaises(ValueError,
+                          analyzer.get_time_delta,
+                          "2015-10-03 19:08:30",
+                          "2015-10-03 19:08:00")
 
 if __name__ == '__main__':
     unittest.main()
--- timetracker_analyzer.py
+++ timetracker_analyzer.py
@@ -1,12 +1,24 @@
 import csv
 
+from datetime import datetime as dt
 
-def total_up_tasks(start_time, end_time):
+
+def get_month_year(date):
+    pass
+
+
+def get_time_delta(start_time, end_time):
     """Return end time minus start time."""
-    # Correct math
     # Handle negative values
     # Handle none values
-    pass
+    date_format = "%Y-%m-%d %H:%M:%S"
+    start_time = dt.strptime(start_time, date_format)
+    end_time = dt.strptime(end_time, date_format)
+
+    if (end_time - start_time).total_seconds() < 0:
+        raise ValueError("Start Time must be before End Time")
+
+    return end_time - start_time
 
 
 def analyze_data(filename):
@@ -18,4 +30,10 @@ def analyze_data(filename):
     # call total up tasks
     # Add it to the month total for that task
     # If no end time, add it as an activity
-    pass
+    with open(filename) as data:
+        reader = csv.reader(data)
+        for row in data:
+            pass
+
+
+analyze_data('timetracker-2015-10-03-19-30-48.csv')
