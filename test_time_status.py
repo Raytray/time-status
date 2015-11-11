1712c0fafe7204c1e2d9c46e2d9856d0ba2d9d46
Raymond Tang <raymond@tang.me>
2015-11-10 20:28:55 -0500

    update tests to match api
    * also be more pythonic by not repeating code and not using globals

--- test_time_status.py
+++ test_time_status.py
@@ -1,25 +1,29 @@
 import unittest
 import time_status as loader
 
+from datetime import datetime as dt
+
 
 class TestTimeStatus(unittest.TestCase):
+    def setUp(self):
+        date_format = "%Y-%m-%d %H:%M:%S"
+        self._start_time = dt.strptime("2015-10-03 19:08:30", date_format)
+        self._end_time = dt.strptime("2015-10-03 19:08:39", date_format)
+
     def test_get_timedelta_perfect(self):
         expected = 9.0
         self.assertEqual(expected,
-                         loader.get_timedelta("2015-10-03 19:08:30",
-                                                 "2015-10-03 19:08:39"))
+                         loader.get_timedelta(self._start_time, self._end_time))
 
     def test_get_timedelta_empty(self):
-        self.assertRaises(ValueError,
+        self.assertRaises(TypeError,
                           loader.get_timedelta,
-                          "2015-10-03 19:08:30",
-                          "")
+                          self._start_time, None)
 
     def test_get_timedelta_negative(self):
         self.assertRaises(ValueError,
-                          loader.get_timedelta,
-                          "2015-10-03 19:08:30",
-                          "2015-10-03 19:08:00")
+                          loader.get_timedelta, self._end_time,
+                          self._start_time)
 
 if __name__ == '__main__':
     unittest.main()
