69897ebe035c87d46a5f427fddeb42b39ddfd94d
Raymond Tang <raymond@tang.me>
2015-11-10 20:38:25 -0500

    test mongo connection

--- time_api_controller.py
+++ time_api_controller.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 @app.route('/api/time-status')
 def get_data():
     config_collection = timetracker_db.get_config_collection()
-    return config_collection.find()
+    return str(config_collection.find_one())
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
