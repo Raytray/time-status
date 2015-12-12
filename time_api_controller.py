1412bc46a7b411bb7b51a073fb890dbcb2d13e8d
Raymond Tang <raymond@tang.me>
2015-12-12 07:20:35 -0500

    initial commit for starting d3 work

--- time_api_controller.py
+++ time_api_controller.py
@@ -2,7 +2,7 @@ import json
 import timetracker_db
 
 from bson import json_util
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 
 app = Flask(__name__)
@@ -13,6 +13,12 @@ def get_data():
     """Time series Handler. Returns the times collection."""
     times_collection = timetracker_db.get_times_collection()
     return json.dumps(times_collection.find_one(), default=json_util.default)
+
+
+@app.route('/')
+def index():
+    """Serve the template."""
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
