691d0da2f5e37aa28cea8ea77b119954e74773ef
Raymond Tang <raymond@tang.me>
2016-01-23 17:13:31 -0500

    add gym and work data

--- static/js/time-status.js
+++ static/js/time-status.js
@@ -2,7 +2,7 @@ var app = angular.module("timeStatus", ['nvd3ChartDirectives']);
 
 app.controller('bulletChartController', function($scope, $http) {
     var convertSecondsToHours = function(seconds) {
-        return seconds/60.0/60;
+        return (seconds/60.0/60).toFixed(2);
     }
 
 
@@ -31,6 +31,64 @@ app.controller('bulletChartController', function($scope, $http) {
                     var maxRange = Math.max(period1Hours, period2Hours) * 1.1;
                     $scope.screenData = {
                         "title": "Screen Data",
+                        "subtitle": "Measured in hours",
+                        "ranges": [0, maxRange],
+                        "markers": [period2Hours],
+                        "measures": [period1Hours]
+                    };
+                });
+        });
+
+    $scope.gymData = {};
+
+    $http.get(getUrl('1m', 'Gym'))
+        .then(function(response) {
+            var period1Seconds = 0;
+            response.data.times.forEach(function(time) {
+                period1Seconds += time.seconds;
+            });
+            var period1Hours = convertSecondsToHours(period1Seconds);
+            $scope.gymData["measures"] = [period1Hours];
+            $http.get(getUrl('2m', 'Gym'))
+                .then(function(response) {
+                    var period2Seconds = 0;
+                    response.data.times.forEach(function(time) {
+                        period2Seconds += time.seconds;
+                    });
+                    period2Seconds = period2Seconds - period1Seconds;
+                    var period2Hours = convertSecondsToHours(period2Seconds);
+                    var maxRange = Math.max(period1Hours, period2Hours) * 1.1;
+                    $scope.gymData = {
+                        "title": "Gym Data",
+                        "subtitle": "Measured in hours",
+                        "ranges": [0, maxRange],
+                        "markers": [period2Hours],
+                        "measures": [period1Hours]
+                    };
+                });
+        });
+
+    $scope.workData = {};
+
+    $http.get(getUrl('1m', 'Work'))
+        .then(function(response) {
+            var period1Seconds = 0;
+            response.data.times.forEach(function(time) {
+                period1Seconds += time.seconds;
+            });
+            var period1Hours = convertSecondsToHours(period1Seconds);
+            $scope.workData["measures"] = [period1Hours];
+            $http.get(getUrl('2m', 'Work'))
+                .then(function(response) {
+                    var period2Seconds = 0;
+                    response.data.times.forEach(function(time) {
+                        period2Seconds += time.seconds;
+                    });
+                    period2Seconds = period2Seconds - period1Seconds;
+                    var period2Hours = convertSecondsToHours(period2Seconds);
+                    var maxRange = Math.max(period1Hours, period2Hours) * 1.1;
+                    $scope.workData = {
+                        "title": "Work Data",
                         "subtitle": "Measured in hours",
                         "ranges": [0, maxRange],
                         "markers": [period2Hours],
