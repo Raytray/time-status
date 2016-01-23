79cde27186579966f289c0b56f71c037c16b6c76
Raymond Tang <raymond@tang.me>
2016-01-23 16:13:31 -0500

    update screendata graph to represent past data
    * marker represents past month's data.

--- static/js/time-status.js
+++ static/js/time-status.js
@@ -5,17 +5,32 @@ app.controller('bulletChartController', function($scope, $http) {
 
     $http.get('api/time-series?period=1m&category=Screen')
         .then(function(response) {
-            total = 0;
+            var period1Seconds = 0;
             response.data.times.forEach(function(time) {
-                total += time.seconds;
+                period1Seconds += time.seconds;
             });
-            total = total / 60.0 / 24;
-            console.log(total);
-            $scope.screenData = {
-                "measures": [total],
-                "title": "Screen Data",
-                "subtitle": "Measured in hours",
-                "ranges": [0, 500]
-            };
+            var period1Hours = convertSecondsToHours(period1Seconds);
+            $scope.screenData["measures"] = [period1Hours];
+            $http.get('api/time-series?period=2m&category=Screen')
+                .then(function(response) {
+                    var period2Seconds = 0;
+                    response.data.times.forEach(function(time) {
+                        period2Seconds += time.seconds;
+                    });
+                    period2Seconds = period2Seconds - period1Seconds;
+                    var period2Hours = convertSecondsToHours(period2Seconds);
+                    var maxRange = Math.max(period1Hours, period2Hours) * 1.1;
+                    $scope.screenData = {
+                        "title": "Screen Data",
+                        "subtitle": "Measured in hours",
+                        "ranges": [0, maxRange],
+                        "markers": [period2Hours],
+                        "measures": [period1Hours]
+                    };
+                });
         });
+
+    var convertSecondsToHours = function(seconds) {
+        return seconds/60.0/60;
+    }
 });
