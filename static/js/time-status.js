5e811c42c702606acba135b4fe06d27413ea7c29
Raymond Tang <raymond@tang.me>
2016-01-23 17:05:26 -0500

    use function to get url

--- static/js/time-status.js
+++ static/js/time-status.js
@@ -1,9 +1,18 @@
 var app = angular.module("timeStatus", ['nvd3ChartDirectives']);
 
 app.controller('bulletChartController', function($scope, $http) {
+    var convertSecondsToHours = function(seconds) {
+        return seconds/60.0/60;
+    }
+
+
+    var getUrl = function(period, category) {
+        return 'api/time-series?period=' + period + '&category=' + category;
+    }
+
     $scope.screenData = {};
 
-    $http.get('api/time-series?period=1m&category=Screen')
+    $http.get(getUrl('1m', 'Screen'))
         .then(function(response) {
             var period1Seconds = 0;
             response.data.times.forEach(function(time) {
@@ -11,7 +20,7 @@ app.controller('bulletChartController', function($scope, $http) {
             });
             var period1Hours = convertSecondsToHours(period1Seconds);
             $scope.screenData["measures"] = [period1Hours];
-            $http.get('api/time-series?period=2m&category=Screen')
+            $http.get(getUrl('2m', 'Screen'))
                 .then(function(response) {
                     var period2Seconds = 0;
                     response.data.times.forEach(function(time) {
@@ -29,8 +38,4 @@ app.controller('bulletChartController', function($scope, $http) {
                     };
                 });
         });
-
-    var convertSecondsToHours = function(seconds) {
-        return seconds/60.0/60;
-    }
 });
