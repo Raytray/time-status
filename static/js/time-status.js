269daee48e6a5413a3945dc3e3a0d7760e99a293
Raymond Tang <raymond@tang.me>
2016-01-23 17:20:03 -0500

    add additional datapoints

--- static/js/time-status.js
+++ static/js/time-status.js
@@ -96,4 +96,91 @@ app.controller('bulletChartController', function($scope, $http) {
                     };
                 });
         });
+
+    $scope.drivingData = {};
+
+    $http.get(getUrl('1m', 'Driving'))
+        .then(function(response) {
+            var period1Seconds = 0;
+            response.data.times.forEach(function(time) {
+                period1Seconds += time.seconds;
+            });
+            var period1Hours = convertSecondsToHours(period1Seconds);
+            $scope.drivingData["measures"] = [period1Hours];
+            $http.get(getUrl('2m', 'Driving'))
+                .then(function(response) {
+                    var period2Seconds = 0;
+                    response.data.times.forEach(function(time) {
+                        period2Seconds += time.seconds;
+                    });
+                    period2Seconds = period2Seconds - period1Seconds;
+                    var period2Hours = convertSecondsToHours(period2Seconds);
+                    var maxRange = Math.max(period1Hours, period2Hours) * 1.1;
+                    $scope.drivingData = {
+                        "title": "Driving Data",
+                        "subtitle": "Measured in hours",
+                        "ranges": [0, maxRange],
+                        "markers": [period2Hours],
+                        "measures": [period1Hours]
+                    };
+                });
+        });
+
+    $scope.sportrockData = {};
+
+    $http.get(getUrl('1m', 'Sportrock'))
+        .then(function(response) {
+            var period1Seconds = 0;
+            response.data.times.forEach(function(time) {
+                period1Seconds += time.seconds;
+            });
+            var period1Hours = convertSecondsToHours(period1Seconds);
+            $scope.sportrockData["measures"] = [period1Hours];
+            $http.get(getUrl('2m', 'Sportrock'))
+                .then(function(response) {
+                    var period2Seconds = 0;
+                    response.data.times.forEach(function(time) {
+                        period2Seconds += time.seconds;
+                    });
+                    period2Seconds = period2Seconds - period1Seconds;
+                    var period2Hours = convertSecondsToHours(period2Seconds);
+                    var maxRange = Math.max(period1Hours, period2Hours) * 1.1;
+                    $scope.sportrockData = {
+                        "title": "Sportrock Data",
+                        "subtitle": "Measured in hours",
+                        "ranges": [0, maxRange],
+                        "markers": [period2Hours],
+                        "measures": [period1Hours]
+                    };
+                });
+        });
+
+    $scope.homeData = {};
+
+    $http.get(getUrl('1m', 'Home'))
+        .then(function(response) {
+            var period1Seconds = 0;
+            response.data.times.forEach(function(time) {
+                period1Seconds += time.seconds;
+            });
+            var period1Hours = convertSecondsToHours(period1Seconds);
+            $scope.homeData["measures"] = [period1Hours];
+            $http.get(getUrl('2m', 'Home'))
+                .then(function(response) {
+                    var period2Seconds = 0;
+                    response.data.times.forEach(function(time) {
+                        period2Seconds += time.seconds;
+                    });
+                    period2Seconds = period2Seconds - period1Seconds;
+                    var period2Hours = convertSecondsToHours(period2Seconds);
+                    var maxRange = Math.max(period1Hours, period2Hours) * 1.1;
+                    $scope.homeData = {
+                        "title": "Home Data",
+                        "subtitle": "Measured in hours",
+                        "ranges": [0, maxRange],
+                        "markers": [period2Hours],
+                        "measures": [period1Hours]
+                    };
+                });
+        });
 });
