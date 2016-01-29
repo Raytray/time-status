d857c6baead9532fc21d38c07e78cc4074056669
Raymond Tang <raymond@tang.me>
2016-01-28 19:32:49 -0500

    add mongo authentication

--- timetracker_db.py
+++ timetracker_db.py
@@ -1,7 +1,16 @@
+import configparser
+
 from pymongo import MongoClient
 
 
+config = configparser.ConfigParser()
+config.read('config.conf')
+
+USERNAME = config.get('mongo', 'USERNAME')
+PASSWORD = config.get('mongo', 'PASSWORD')
+
 client = MongoClient()
+client.timetracker.authenticate(USERNAME, PASSWORD)
 
 
 def get_config_collection():
